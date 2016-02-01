34918ed42e2e335094f09ef19ad4c12a4e0093d8
elvissieux <elvissieux@gmx.com>
2016-02-01 09:23:07 +0100

    Add comments

--- wordcount_locally_testable.py
+++ wordcount_locally_testable.py
@@ -2,11 +2,13 @@
 import sys 
 import re 
 
-filepath = "/media/t0135444/DATA/wordcount/input/0010"
+#change with a test file here
+filepath = "your_path_here"
 
 def main(argv): 
     print argv
     pattern = re.compile("[a-zA-Z][a-zA-Z0-9]*")
+    #switch between local and hadoop cluster prod (launch with any argument for local test)
     for line in [open(filepath), sys.stdin, ][len(argv)==1]:
         for word in pattern.findall(line):
             print "LongValueSum:" + word.lower() + "\t" + "1" 
